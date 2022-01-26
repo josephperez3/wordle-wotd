2bd7547c914cd029ca37cdeef7005ec204cf270a
Joseph Perez <perezj0230@gmail.com>
2022-01-26 02:44:39 -0500

    made website

---
+++ algo.js
@@ -0,0 +1,22 @@
+import wordList from "./wordlist.js";
+
+var htmlDate = document.getElementById("date");
+var htmlWord = document.getElementById("word");
+
+const date = new Date();
+var Ha = new Date(2021, 5, 19, 0, 0, 0, 0);
+function Na(e, a) {
+  var s = new Date(e),
+    t = new Date(a).setHours(0, 0, 0, 0) - s.setHours(0, 0, 0, 0);
+  return Math.round(t / 864e5);
+}
+function Da(date) {
+  var index,
+    s = Na(Ha, date);
+  return (index = s % wordList.length), wordList[index];
+}
+
+console.log(Da(date));
+
+htmlDate.innerText = date;
+htmlWord.innerText = Da(date);
